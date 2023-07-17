9bee910dedeebb3467aade0654d021555bf7ad92
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-07-17 17:47:12 +0300

    clean the components

--- src/components/Sheet.jsx
+++ src/components/Sheet.jsx
@@ -1,50 +1,81 @@
 import React from "react";
-// import PlayOff from "./PlayOff";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function Sheet({ teams, sheetData }) {
-  if (teams[0] !== undefined) {
+function Sheet({ teams }) {
+  teams = teams[0];
+  if (teams !== undefined) {
     let sortTeams = teams;
     let i = 0;
+    let playOff = false;
 
-    sortTeams[0].sort((a, b) => b.points - a.points);
-    return (
-      <div className="text-center fixed-top" id="sheet">
-        <table className="table table-hover table-bordered">
-          <thead className="bg-body-secondary">
-            <th scope="col">#</th>
-            <th scope="col">Team</th>
-            <th scope="col">Team Rating</th>
-            <th scope="col">Games</th>
-            <th scope="col">Points</th>
-            <th scope="col">GF</th>
-            <th scope="col">GA</th>
-          </thead>
-          {sortTeams[0].map((team) => {
-            i++;
-            return (
-              <tbody key={team.id}>
-                <tr>
-                  <th>{i}</th>
-                  <th>{team.name}</th>
-                  <th>{team.rating * 100}</th>
-                  <th>{team.game_counter}</th>
-                  <th className="">{team.points}</th>
-                  <th className="">{team.goals_for}</th>
-                  <th className="">{team.goals_against}</th>
-                </tr>
-              </tbody>
-            );
-          })}
-        </table>
-        {/* <div className={style}>
-          <PlayOff teams={teams[0]} />
-        </div> */}
-        {/* <button className={style} onClick={() => sheetData(teams[0])}>
-          Let's start Play-Off
-        </button> */}
-      </div>
-    );
+    playOff = teams.every((team) => team.game_counter === 1);
+
+    if (playOff === true) {
+      return (
+        <div className="text-center fixed-top">
+          <h1>Play Off</h1>
+          <table className="table table-hover table-bordered">
+            <thead className="bg-body-secondary">
+              <th scope="col">#</th>
+              <th scope="col">Team</th>
+              <th scope="col">Team Rating</th>
+              <th scope="col">GF</th>
+              <th scope="col">GA</th>
+            </thead>
+            {/* {teams[0].map((team) => {
+              return (
+                <tbody key={team.id}>
+                  <tr>
+                    <th></th>
+                    <th>{team.name}</th>
+                    <th>{team.rating * 100}</th>
+                    <th className="">{team.goals_for}</th>
+                    <th className="">{team.goals_against}</th>
+                  </tr>
+                </tbody>
+              );
+            })} */}
+            {/* {teams[0].find(
+          (element) => element.abbreviation === 
+        )} */}
+            {console.log(teams[0])}
+          </table>
+        </div>
+      );
+    } else {
+      sortTeams.sort((a, b) => b.points - a.points);
+      return (
+        <div className="text-center fixed-top" id="sheet">
+          <table className="table table-hover table-bordered">
+            <thead className="bg-body-secondary">
+              <th scope="col">#</th>
+              <th scope="col">Team</th>
+              <th scope="col">Team Rating</th>
+              <th scope="col">Games</th>
+              <th scope="col">Points</th>
+              <th scope="col">GF</th>
+              <th scope="col">GA</th>
+            </thead>
+            {sortTeams.map((team) => {
+              i++;
+              return (
+                <tbody key={team.id}>
+                  <tr>
+                    <th>{i}</th>
+                    <th>{team.name}</th>
+                    <th>{team.rating * 100}</th>
+                    <th>{team.game_counter}</th>
+                    <th className="">{team.points}</th>
+                    <th className="">{team.goals_for}</th>
+                    <th className="">{team.goals_against}</th>
+                  </tr>
+                </tbody>
+              );
+            })}
+          </table>
+        </div>
+      );
+    }
   }
 }
 
