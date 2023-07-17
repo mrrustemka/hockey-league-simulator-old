b69e6b024c5ad6aab1a553cfeae5a293420dd287
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-07-17 17:56:09 +0300

    delete Calendar GameList components add League component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import Calendar from "./components/Calendar";
+// import Calendar from "./components/Calendar";
+import GameList from "./components/League";
 
 function App() {
   return (
     <div className="App mb-4">
-      <Calendar />
+      <GameList />
     </div>
   );
 }
--- src/components/League.jsx
+++ src/components/League.jsx
@@ -3,10 +3,49 @@ import Game from "./Game";
 import Sheet from "./Sheet";
 import teamsInfo from "../data/teamsInfo";
 import PlayOffGame from "./PlayOff";
+// import teamsInfo from "../data/teamsInfo";
+import { v4 as uuidv4 } from "uuid";
 
-function GameList({ games }) {
+let teamsList = "";
+let teams;
+let schedule = [{ home: "", away: "" }];
+
+teamsInfo.map((teamInfo) => {
+  teamsList += teamInfo.abbreviation + " ";
+  return teamsList;
+});
+
+teams = teamsList.split(" ", teamsList.split(" ").length - 1);
+
+let i = 0;
+teamsInfo.map((teamInfo) => {
+  teams.map((team) => {
+    if (teamInfo.abbreviation === team) {
+      return team;
+    } else {
+      let game = { id: uuidv4(), home: teamInfo.abbreviation, away: team };
+      if (
+        schedule.find(
+          (element) => element.home === game.away && element.away === game.home
+        )
+      ) {
+        return team;
+      } else {
+        schedule[i] = game;
+      }
+    }
+    i++;
+    return team;
+  });
+  for (let k = schedule.length - 1; k > 0; k--) {
+    let j = Math.floor(Math.random() * (k + 1));
+    [schedule[k], schedule[j]] = [schedule[j], schedule[k]];
+  }
+  return teamInfo;
+});
+
+function GameList() {
   let [teams, setTeams] = useState([]);
-  let playOff = false;
 
   function sheetData(newTeams) {
     setTeams([newTeams]);
@@ -36,7 +75,7 @@ function GameList({ games }) {
       </div>
       <Sheet teams={teams} />
       <div id="games">
-        {games.map((game) => {
+        {schedule.map((game) => {
           let homeTeam = teamsInfo.find(
             (element) => element.abbreviation === game.home
           );
